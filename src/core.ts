1cc25e4e9b69fe809d9dac9278a27ef4694c97c8
moritzbrantner <31051084+moritzbrantner@users.noreply.github.com>
2026-08-26 17:34:28 +0200

    Expose chart view state and owned WASM loader from core

--- src/core.ts
+++ src/core.ts
@@ -3,3 +3,5 @@
 export * from "./density";
 export * from "./analytics";
 export * from "./labels";
+export * from "./view-state";
+export { getLoadedChartWasmKernel, loadChartWasmKernel } from "./wasm-kernel";
